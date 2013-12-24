d06a24c0bbc3839f578a0a7c2691e264d88719c4
Charles Butler <chuck@dasroot.net>
2013-12-24 04:30:59 -0500

    adding charm skeleton

---
+++ README.ex
@@ -0,0 +1,38 @@
+# Starbound
+
+-- description
+
+# Usage
+
+Step by step instructions on using the charm:
+
+    juju deploy servicename
+
+## Scale out Usage
+
+If the charm has any recommendations for running at scale, outline them in examples here. For example if you have a memcached relation that improves performance, mention it here. 
+
+## Known Limitations and Issues
+
+This not only helps users but gives people a place to start if they want to help you add features to your charm. 
+
+# Configuration
+
+The configuration options will be listed on the charm store, however If you're making assumptions or opinionated decisions in the charm (like setting a default administrator password), you should detail that here so the user knows how to change it immediately, etc.
+
+# Contact Information
+
+Though this will be listed in the charm store itself don't assume a user will know that, so include that information here:
+
+## Upstream Project Name
+
+- Upstream website
+- Upstream bug tracker
+- Upstream mailing list or contact information
+- Feel free to add things if it's useful for users
+
+## Charm Contact
+
+Though the charm store lists a maintainer, a URL, and where to report bugs you might want to use this area to expand on different contact information if you want. 
+
+* Be sure to remove the templated parts before submitting to https://launchpad.net/charms for inclusion in the charm store.
